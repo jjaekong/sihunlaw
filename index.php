e2ddb75886a70d00668fa456a4c77f00fdebf49e
kyh5392 <kyh90495392@gmail.com>
2016-07-06 14:44:56 +0900

    메인페이지 gnb 메뉴 작업 #2

--- index.php
+++ index.php
@@ -8,10 +8,12 @@
 	<link href="/css/bootstrap.min.css" rel="stylesheet">
 	<link href="/css/default.css" rel="stylesheet">
     <link href="/css/main.css" rel="stylesheet">
+    <link href="/css/header.css" rel="stylesheet">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<script type="text/javascript" src="/js/jquery.bxslider.js"></script>
 	<script type="text/javascript" src="/js/jquery.bxslider.min.js"></script>
 	<script type="text/javascript" src="/js/bootstrap.min.js"></script>
+    <script src="/js/common.js"></script>
 </head>
 <body class="main">
 	<a href="#gnb" class="skip-nav sr-only sr-only-focusable">주메뉴로 건너뛰기</a>
@@ -20,13 +22,59 @@
 		<div class="container">
 			<h1><a href="#"><img src="/images/main/logo.jpg" alt="로고"></a></h1>
 			<nav id="gnb" tabindex="-1">
-				<ul>
-					<li><a href="#">시헌소개</a></li>
-					<li><a href="#">업무분야</a></li>
-					<li><a href="#">구성원</a></li>
-					<li><a href="#">소식 및 자료</a></li>
-					<li><a href="#">관련사이트</a></li>
-				</ul>
+				<div class="wrapper">
+					<ul>
+						<li>
+							<a href="#">시헌소개</a>
+							<ul>
+								<li><a href="#">법인소개</a></li>
+								<li><a href="#">비전과 가치</a></li>
+								<li><a href="#">오시는 길</a></li>
+							</ul>
+						</li>
+						<li>
+							<a href="#">업무분야</a>
+							<ul>
+								<li><a href="#">기업법무</a></li>
+								<li><a href="#">VENTURE/IPO</a></li>
+								<li><a href="#">M&A</a></li>
+								<li><a href="#">금융/증권</a></li>
+								<li><a href="#">정보보호</a></li>
+								<li><a href="#">외투법인 설립</a></li>
+								<li><a href="#">국제/해외업무</a></li>
+								<li><a href="#">민사소송</a></li>
+								<li><a href="#">형사소송</a></li>
+								<li><a href="#">행정소송</a></li>
+								<li><a href="#">가사소송</a></li>
+							</ul>
+						</li>
+						<li>
+							<a href="#">구성원</a>
+							<ul>
+								<li><a href="#">한국변호사</a></li>
+								<li><a href="#">외국변호사</a></li>
+								<li><a href="#">고문</a></li>
+							</ul>
+						</li>
+						<li>
+							<a href="#">소식 및 자료</a>
+							<ul>
+								<li><a href="#">소식</a></li>
+								<li><a href="#">자료</a></li>
+								<li><a href="#">뉴스레터</a></li>
+								<li><a href="#">블로그</a></li>
+							</ul>
+						</li>
+						<li>
+							<a href="#">관련사이트</a>
+							<ul>
+								<li><a href="#">홍콩 로펌</a></li>
+								<li><a href="#">중국 로펌</a></li>
+								<li><a href="#">폴란드 로펌</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div>
 			</nav>
 			<ul class="lang">
 				<li>CONTACT US</li>
--- test_header.php
+++ test_header.php
@@ -6,6 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>법무법인시헌</title>
 	<link href="/css/bootstrap.min.css" rel="stylesheet">
+	<link href="/css/test_header.css" rel="stylesheet">
 
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
@@ -18,53 +19,57 @@
 	<a href="#content" class="skip-nav sr-only sr-only-focusable">컨텐츠로 건너뛰기</a>
 	<header id="header">
 		<div class="container">
-			<h1><a href="#"><img src="/images/logo.jpg" alt="로고"></a></h1>
+			<h1><a href="#"><img src="/images/main/logo.jpg" alt="로고"></a></h1>
 			<nav id="gnb" tabindex="-1">
 				<div class="wrapper">
 					<ul>
 						<li>
 							<a href="#">시헌소개</a>
 							<ul>
-								<li><a href="#">메뉴1</a></li>
-								<li><a href="#">메뉴2</a></li>
-								<li><a href="#">메뉴3</a></li>
-								<li><a href="#">메뉴4</a></li>
+								<li><a href="#">법인소개</a></li>
+								<li><a href="#">비전과 가치</a></li>
+								<li><a href="#">오시는 길</a></li>
 							</ul>
 						</li>
 						<li>
 							<a href="#">업무분야</a>
 							<ul>
-								<li><a href="#">메뉴1</a></li>
-								<li><a href="#">메뉴2</a></li>
-								<li><a href="#">메뉴3</a></li>
-								<li><a href="#">메뉴4</a></li>
+								<li><a href="#">기업법무</a></li>
+								<li><a href="#">VENTURE/IPO</a></li>
+								<li><a href="#">M&A</a></li>
+								<li><a href="#">금융/증권</a></li>
+								<li><a href="#">정보보호</a></li>
+								<li><a href="#">외투법인 설립</a></li>
+								<li><a href="#">국제/해외업무</a></li>
+								<li><a href="#">민사소송</a></li>
+								<li><a href="#">형사소송</a></li>
+								<li><a href="#">행정소송</a></li>
+								<li><a href="#">가사소송</a></li>
 							</ul>
 						</li>
 						<li>
 							<a href="#">구성원</a>
 							<ul>
-								<li><a href="#">메뉴1</a></li>
-								<li><a href="#">메뉴2</a></li>
-								<li><a href="#">메뉴3</a></li>
-								<li><a href="#">메뉴4</a></li>
+								<li><a href="#">한국변호사</a></li>
+								<li><a href="#">외국변호사</a></li>
+								<li><a href="#">고문</a></li>
 							</ul>
 						</li>
 						<li>
 							<a href="#">소식 및 자료</a>
 							<ul>
-								<li><a href="#">메뉴1</a></li>
-								<li><a href="#">메뉴2</a></li>
-								<li><a href="#">메뉴3</a></li>
-								<li><a href="#">메뉴4</a></li>
+								<li><a href="#">소식</a></li>
+								<li><a href="#">자료</a></li>
+								<li><a href="#">뉴스레터</a></li>
+								<li><a href="#">블로그</a></li>
 							</ul>
 						</li>
 						<li>
 							<a href="#">관련사이트</a>
 							<ul>
-								<li><a href="#">메뉴1</a></li>
-								<li><a href="#">메뉴2</a></li>
-								<li><a href="#">메뉴3</a></li>
-								<li><a href="#">메뉴4</a></li>
+								<li><a href="#">홍콩 로펌</a></li>
+								<li><a href="#">중국 로펌</a></li>
+								<li><a href="#">폴란드 로펌</a></li>
 							</ul>
 						</li>
 					</ul>
@@ -74,125 +79,12 @@
 				<li>CONTACT US</li>
 				<li>
 					<ul>
-						<li><a href="#"><img src="/images/ico_flag_us.png" alt="" /></a></li>
-						<li><a href="#"><img src="/images/ico_flag_cn.png" alt="" /></a></li>
+						<li><a href="#"><img src="/images/main/ico_flag_us.png" alt="" /></a></li>
+						<li><a href="#"><img src="/images/main/ico_flag_cn.png" alt="" /></a></li>
 					</ul>
 				</li>
 			</ul>
 		</div>
 	</header>
-	
 </body>
-<style type="text/css">
-ul,ol {
-	list-style:none;
-	padding:0;
-	margin:0;
-}
-body {
-	overflow-y: scroll;
-	min-width:1200px;
-	margin:0 auto;
-	background-color:#edf2f8;
-}
-#header {
-	border-bottom:3px solid #283b73;
-	background-color:#fff;
-	height:88px;
-}
-#header .container {
-	position:relative;
-	width:1200px;
-	background-color:#fff;
-}
-#header .container h1 {
-	position:absolute;
-	top:19px;
-	left:15px;
-	margin:0;
-}
-
-#gnb {
-	position:absolute;
-	top:0;
-	left:250px;
-	z-index:9100;
-	height:86px;
-	overflow:hidden;
-	margin:0 auto;
-	background-color:#edf2f8;
-}
-#gnb.opened {
-	height:auto; 
-	background-color:#edf2f8;
-}
-#gnb.opened .wrapper > ul > li > ul {
-	display:block; 
-}
-#gnb .wrapper {
-	height:88px;
-	background-color:#fff; 
-	border-bottom:3px solid #283b73;
-}
-#gnb .wrapper > ul > li {
-	float:left;
-}
-#gnb .wrapper > ul > li > a {
-	width:145px;
-	display:block;
-	font-size:17px;
-	color:#333;
-	text-align:center;
-	line-height:85px;
-	text-decoration:none; 
-	font-weight:600;
-}
-#gnb .wrapper > ul > li > ul {
-	display:none;
-	position:relative;
-	padding:10px 0;
-	height:100%; 
-}
-#gnb .wrapper > ul > li > ul > li > a {
-	display:block;
-	text-align:center;
-	font-size:14px;
-	color:#666;
-	line-height:30px;
-}
-#gnb .wrapper > ul > li > ul > li > a:hover {
-	text-decoration:underline !important;
-}
-
-#gnb .wrapper > ul > li.active, #gnb .wrapper > ul > li:hover {
-	background-color:#283b73;
-}
-#gnb .wrapper > ul > li.active > a, #gnb .wrapper > ul > li:hover > a {
-	color:#fff; 
-}
-#gnb .wrapper > ul > li.active > ul > li > a, #gnb .wrapper > ul > li:hover > ul > li > a {
-	color:#fff; 
-}
-
-#header .container .lang {
-	position:absolute;
-	top:0;
-	right:15px;
-	z-index:9200;
-}
-#header .container .lang > li {
-	display:inline-block;
-	font-size:12px;
-	color:#999;
-	line-height: 85px;
-}
-#header .container .lang > li:first-child:after {
-	content:"|";
-	padding:0 10px;
-}
-#header .container .lang > li ul li {
-	display:inline-block;
-}
-
-</style>
 </html>
